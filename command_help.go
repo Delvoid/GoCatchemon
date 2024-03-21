832e9ebb45b909aa8b0710ac0030ce89c88c2cb0
Delvoid <delvoid.dev@gmail.com>
2024-03-21 21:29:17 +0000

    loop over commands

--- command_help.go
+++ command_help.go
@@ -5,7 +5,9 @@ import "fmt"
 func commandHelp() error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
-	fmt.Println("help: Displays a help message")
-	fmt.Println("exit: Exits the Pokedex")
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s - %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
 	return nil
 }
